54b7731fee34757421e9e3c6c50aab33056d44c3
Germano <ochner14@gmail.com>
2025-05-11 15:52:27 -0300

    Primeira versão

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,33 @@
-#Comentário
-#teste 2    
+# Projeto #1 - Calculadora
+
+# mensagem1
+print("Projeto #1 - Calculadora")
+print("Você pode somar (+), subtrair (-), multiplicar (*) ou dividir (/)")
+
+# Pergunta o primeiro número
+num1 = float(input("Digite o primeiro número: "))
+#Lembrando que o input serve para pedir ao usuário digitar algo
+#O float faz com que a num1 seja um número decimal
+
+# Pergunta a operação
+operacao = input("Digite a operação (+, -, *, /): ")
+
+# Pergunta o segundo número
+num2 = float(input("Digite o segundo número: "))
+
+#Cálculo
+if operacao == "+":
+    result = num1 + num2
+elif operacao == "-":
+    result = num1 - num2
+elif operacao == "*":
+    result = num1 * num2
+elif operacao == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Erro! Divisão por zero."
+else:
+    result = "Operação inválida."
+
+print("Resultado:", result) 
